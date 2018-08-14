e9679cebe0552139e5f35cf03730e27277cf829e
Stephen <stephendeanharper@gmail.com>
2018-08-13 19:29:43 -0700

    added more functionality, added visual and readme

---
+++ config_files/README.md
@@ -0,0 +1,21 @@
+# Lab 01: Intro to Python
+
+**Author**: Stephen Harper
+**Version**: 1.0.0
+**Github**: https://github.com/indigoshock
+
+## Overview
+This is a terminal app which poses as a virtual restaurant called Snakes cafe.
+
+## Getting Started
+The steps a user must take to get this app up and running on their machine is to download VS Code. Download the Python extension. And download Git Bash or Terminal. One way to run this app is to clone this repo on github.
+
+## Architecture
+The technologies used are the python language. VS Code.
+![Lab01](../asset/Lab01.png)
+
+## API
+No API's were harmed in the making of this.
+
+## Change Log
+08-13-2018 7:16pm - Functionality. README with visual and config files added.
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -5,10 +5,20 @@ import sys
 WIDTH = 96
 BANK = [
     { 
-        'question' : 'What appetizer would you like to order?\n',
-        'answer': 'hamburger' or 'cheeseburger' or 'lambburger' or 'cheesecake' or 'applepie' or 'taffy' or 'mozzarellasticks' or 'spinachartichokedip' or 'fries' or 'moscowmule' or 'doublemule' or 'undermule'
+        'question' : 'What food would you like to order?\n',
+        'answer': 'hamburger' or 'cheeseburger' or 'lambburger' or 'cheesecake' or 'applepie' or 'taffy' or 'mozzarellasticks' or 'spinachartichokedip' or 'fries' or 'moscowmule' or 'doublemule' or 'undermule',
+        'status': False
     }
         ]
+
+
+def menu():
+    print(dedent(f'''
+    Appetizers:\n {'-'*10}\n Mozzarella Sticks\n Spinach Artichoke Dip\n Fries\n {'-'*10}\n
+    Entrees:\n {'-'*10}\n Hamburger\n Cheeseburger\n Lamb Burger\n {'-'*10}\n
+    Desserts:\n {'-'*10}\n Cheesecake\n Apple Pie\n Taffy\n {' '*10}
+    Drinks:\n {'-'*10}\n Moscow Mule\n Double Mule\n Under Mule\n {' '*10}
+    '''))
 
 
 def greeting():  
@@ -24,29 +34,26 @@ def ask_question(question):
 
 
 def check_input(user_in, item):
-    if user_in.lower() == 'quit':
+    if user_in.lower().replace(" ", "") == 'quit':
         exit()
         return
 
-    if user_in.lower() == item['answer'].lower().replace(" ",""):
-        return
+    if user_in.lower().replace(" ","") == item['answer'].lower().replace(" ",""):
+        item['status'] = True
+        return True
 
 
-def menu():
-    print(dedent(f'''
-    Appetizers:\n {'-'*10}\n Mozzarella Sticks\n Spinach Artichoke Dip\n Fries\n {'-'*10}\n
-    Entrees:\n {'-'*10}\n Hamburger\n Cheeseburger\n Lamb Burger\n {'-'*10}\n
-    Desserts:\n {'-'*10}\n Cheesecake\n Apple Pie\n Taffy\n {' '*10}
-    Drinks:\n {'-'*10}\n Moscow Mule\n Double Mule\n Under Mule\n {' '*10}
-    '''))
+def feedback(status):
+    if status is True:
+        print(dedent(f'''
+            You ordered a food item! I'll bring it out right away.\n
+        '''))
+        ordering_process()
 
-
-def run():
-    greeting()
-    menu()
-    for item in BANK:
-        while item != 0:
-        user_in = ask_question(item['question'])
+    else: 
+        print(dedent('''
+            Sorry, I do not understand. Do you mind repeating your order?\n
+        '''))
 
 
 def exit():
@@ -56,6 +63,20 @@ def exit():
     sys.exit()
 
 
+def run():
+    greeting()
+    menu()
+    ordering_process()
+
+
+def ordering_process():
+    for item in BANK:
+        while item['status'] is False:
+            user_in = ask_question(item['question'])
+            status = check_input(user_in, item)
+            feedback(status)
+
+
 if __name__ == '__main__':
     run()
-    
+
